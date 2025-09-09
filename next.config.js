36c8f684c001dcc9aac6ee7c3a38c1fca4e69899
Hassan Ahmad <hassanahmad2136@gmail.com>
2025-09-09 16:45:53 +0500

    Add GitHub Actions workflow for deployment

--- next.config.js
+++ next.config.js
@@ -1,7 +1,21 @@
 /** @type {import('next').NextConfig} */
+const path = require('path')
+
 const nextConfig = {
   images: {
     domains: ['localhost'],
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, 'src'),
+      '@/components': path.resolve(__dirname, 'src/components'),
+      '@/lib': path.resolve(__dirname, 'src/lib'),
+      '@/types': path.resolve(__dirname, 'src/types'),
+      '@/hooks': path.resolve(__dirname, 'src/hooks'),
+      '@/utils': path.resolve(__dirname, 'src/utils'),
+    }
+    return config
   },
 }
 
